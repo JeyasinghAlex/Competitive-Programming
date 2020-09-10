0b9147e87994595750066abbbde2ba6010ea3563
alex <jsinghalex0022@gmail.com>
2020-09-11 02:37:44 +0530

    sep - 10

---
+++ LeetCodeSep/BullsAndCows.java
@@ -0,0 +1,26 @@
+package LeetCodeChallenges.LeetCodeSep;
+
+public class BullsAndCows {
+
+    public String getHint(String secret, String guess) {
+        int[] number = new int[10];
+        int bull = 0, cow = 0;
+
+        for (int i = 0; i < secret.length(); ++i) {
+            int s = secret.charAt(i) - '0';
+            int g = guess.charAt(i) - '0';
+            if (s == g) {
+                ++bull;
+            }else {
+                if (number[s] < 0)
+                    ++cow;
+                if (number[g] > 0)
+                    ++cow;
+
+                number[s]++;
+                number[g]--;
+            }
+        }
+        return bull + "A" + cow + "B";
+    }
+}
