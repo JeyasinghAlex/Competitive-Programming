bd763905434cb37fb8a78bb078be15442f1adde0
alex <jsinghalex0022@gmail.com>
2020-09-11 02:24:21 +0530

    sep - 10

---
+++ LeetCodeSep/Main.java
@@ -0,0 +1,10 @@
+package LeetCodeChallenges.LeetCodeSep;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BullsAndCows obj = new BullsAndCows();
+        String s = obj.getHint("1122", "1222");
+        System.out.println(s);
+    }
+}
