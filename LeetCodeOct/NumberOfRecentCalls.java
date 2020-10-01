44beeaec5b90c7a412a147c63bf976563998a6bc
alex <jsinghalex0022@gmail.com>
2020-10-02 01:03:50 +0530

    sep - 30

---
+++ LeetCodeOct/NumberOfRecentCalls.java
@@ -0,0 +1,20 @@
+package LeetCodeChallenges.LeetCodeOct;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class NumberOfRecentCalls {
+
+    private Queue<Integer> queue;
+    public NumberOfRecentCalls() {
+        queue = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        queue.add(t);
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+        return queue.size();
+    }
+}
