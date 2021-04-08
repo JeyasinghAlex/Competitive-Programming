85e31891b938034f37957ca793817ea11c99b323
jeyasinghalex.i <jeyasinghalex.i@zohocorp.com>
2021-04-08 09:23:18 +0530

    apr-7

--- LeetCodeApr-2021/LongestValidParentheses.java
+++ LeetCodeApr-2021/LongestValidParentheses.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class LongestValidParentheses {
+import java.util.Stack;
+
+public class LongestValidParentheses {
+
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++)
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            }
+            else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                }
+                else ans = Math.max(ans, i - stack.peek());
+            }
+        return ans;
+    }
 }
