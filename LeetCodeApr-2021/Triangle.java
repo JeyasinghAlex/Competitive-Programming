1b0237d016c2ebfe9ab2beccab7ec2ec494cbfad
jeyasinghalex.i <jeyasinghalex.i@zohocorp.com>
2021-04-24 21:11:40 +0530

    apr - 23

--- LeetCodeApr-2021/Triangle.java
+++ LeetCodeApr-2021/Triangle.java
@@ -3,6 +3,21 @@ import java.util.List;
 
 public class Triangle {
 
+    public int minimumTot(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n];
+        for (int i = 0; i < triangle.get(n - 1).size(); ++i) {
+            dp[i] = triangle.get(n - 1).get(i);
+        }
+
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = 0; j < triangle.get(i).size(); ++j) {
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0];
+    }
+
     public int minimumTotal(List<List<Integer>> triangle) {
 
         for (int i = 1; i < triangle.size(); ++i) {
@@ -23,7 +38,6 @@ public class Triangle {
                     curr.set(j, min);
                 }
             }
-
             triangle.set(i, new ArrayList<>(curr));
         }
 
@@ -31,8 +45,6 @@ public class Triangle {
         for (int a : triangle.get(triangle.size() - 1)) {
             min = Math.min(min, a);
         }
-
         return min;
-
     }
 }
