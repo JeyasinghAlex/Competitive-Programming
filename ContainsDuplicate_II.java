c33ed0e6a4bc199aad2c58783c242ed430a71e4f
jeyasinghalex.i <jeyasinghalex.i@zohocorp.com>
2021-04-29 09:29:55 +0530

    apr - 29 - morn

--- ContainsDuplicate_II.java
+++ ContainsDuplicate_II.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class ContainsDuplicate_II {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ContainsDuplicate_II {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.containsKey(nums[i])) {
+                int diff = Math.abs(map.get(nums[i]) - i);
+                if (diff <= k) {
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+
+//    Using a sliding window approach:
+    public boolean containsNearDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(i > k) {
+                set.remove(nums[i-k-1]);
+            }
+            if(!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
