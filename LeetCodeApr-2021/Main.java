5ee9297edc45460c93ff41538c1e26fcc4f58873
jeyasinghalex.i <jeyasinghalex.i@zohocorp.com>
2021-05-10 01:48:41 +0530

    may - 1

--- LeetCodeApr-2021/Main.java
+++ LeetCodeApr-2021/Main.java
@@ -6,30 +6,124 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println(largeGroupPositions("abbxxxxzzy"));
+//        System.out.println(new Main().peakIndexInMountainArray(new int[]{3,4,5,1}));
+//        int a = Arrays.binarySearch(new int[]{1, 3, 5, 5,5,5,5,5, 6}, 5);
+//        System.out.println(a);
+//        System.out.println(new Main().guessNumber(10));
+//        new Main().duplicateZeros(new int[]{1,0,2,3,0,4,5,0});
+        System.out.println(new Main().maximumPopulation(new int[][]{{1950,1961},{1960,1971},{1970,1981}}));
     }
 
-    public static List<List<Integer>> largeGroupPositions(String s) {
+    public int maximumPopulation(int[][] logs) {
 
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 4));
-        List<Integer> list1 = new ArrayList<>(List.of(1, 2, 3, 4));
+        int[] score = new int[101];
 
-        int count = 1, index = 0;
-        for (int i = 1; i < s.length(); ++i) {
-            if (s.charAt(i) == s.charAt(i - 1)) {
-                ++count;
-            } else if (count >= 3) {
-                ans.add(new ArrayList(Arrays.asList(i - count, i - 1)));
-                count = 1;
-            } else {
-                count = 1;
+        for(int[] arr : logs) {
+            score[arr[0] - 1950]++;
+            score[arr[1] - 1950]--;
+        }
+        for (int i = 1; i < score.length; ++i) {
+            score[i] = score[i] + score[i - 1];
+        }
+        System.out.println(Arrays.toString(score));
+
+        int max = Integer.MIN_VALUE;
+        int res = 0;
+
+        for (int i = 0; i < score.length; ++i) {
+            if (score[i] > max) {
+                max = score[i];
+                res = i + 1950;
             }
         }
-        return ans;
+        return res;
     }
 
+    public void duplicateZeros(int[] arr) {
 
+        for (int i = 0; i < arr.length; ++i) {
+            if (arr[i] == 0) {
+
+                for (int j = arr.length - 1; j > i; --j) {
+                    arr[j] = arr[j - 1];
+                }
+                ++i;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public int guessNumber(int n) {
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int res = guess(mid);
+            if (res == 0)
+                return mid;
+            else if (res < 0)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return -1;
+    }
+
+    private int guess(int num) {
+        int pick = 6;
+        return Integer.compare(pick, num);
+    }
+
+    public int peakIndexInMountainArray(int[] arr) {
+
+        int lo = 0;
+        int hi = arr.length;
+
+        while (lo < hi) {
+
+            int mid = lo + hi >>> 1;
+            if (arr[mid] > arr[mid + 1]) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+
+    private int lowerBound(int[] nums, int target){
+        final int N = nums.length;
+        int lo = 0, hi = N;
+        while(lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+
+            if (target > nums[mid])
+                lo = mid + 1;
+            else
+                hi = mid;
+        }
+        return lo;
+    }
+
+    public static int minDistance(String word1, String word2) {
+
+        int[] arr = new int[26];
+
+        for (char a : word1.toCharArray()) {
+            arr[a - 'a']++;
+        }
+
+        for (char a : word2.toCharArray()) {
+            arr[a - 'a']--;
+        }
+
+        int count = 0;
+        for (int i : arr) {
+            count += Math.abs(i);
+        }
+
+        return count;
+    }
 
     public int[] replaceElements(int[] arr) {
         int[] res = new int[arr.length];
