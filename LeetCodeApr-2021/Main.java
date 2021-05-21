29650ac07a0640666096b88732d56501925b65ba
jeyasinghalex.i <jeyasinghalex.i@zohocorp.com>
2021-05-22 01:08:09 +0530

    may - 21

--- LeetCodeApr-2021/Main.java
+++ LeetCodeApr-2021/Main.java
@@ -9,8 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(new Main().checkPalindromeFormation("pvhmupgqeltozftlmfjjde"
-               , "yjgpzbezspnnpszebzmhvp"));
+
     }
 
     public boolean checkPalindromeFormation(String a, String b) {
