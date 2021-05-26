e03535681ab4ca28dc40d1e8057b127886fce34a
jeyasinghalex.i <jeyasinghalex.i@zohocorp.com>
2021-05-26 20:52:35 +0530

    may - 26

--- LeetCodeApr-2021/Main.java
+++ LeetCodeApr-2021/Main.java
@@ -6,8 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String s = "alex";
-        s.toLowerCase();
     }
 
     public boolean checkPalindromeFormation(String a, String b) {
